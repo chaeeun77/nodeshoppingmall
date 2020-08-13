a5e08cffb6fa67b619340efb7b0eef56a1833d58
chaeeun <chaeeun77@naver.com>
2020-08-13 11:37:11 +0900

    add order router

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const app = express(); // ()는 모음을 표시한다.
 // })//req : 사용자의 모든 요청을 추상화시킨것
 
 const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
+//order.js의 내용을 require(불러와서)하고 이를 상수화시킨다. 왜? 내용이 기니까
 
 app.use('/products', productRoutes)
-
+app.use('/orders', orderRoutes)
+//use : 실행시키겠다. orders라는 것을 요청하면 orderRoutes를 실행시키겠다.
 
 const PORT = 5000;
 

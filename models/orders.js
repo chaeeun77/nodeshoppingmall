e4398aff9eebe2588e8e3c0047062aa9a9af69fe
chaeeun <chaeeun77@naver.com>
2020-08-16 19:34:35 +0900

    add order model

---
+++ models/orders.js
@@ -0,0 +1,11 @@
+//1번
+const mongoose = require('mongoose'); //mongoose
+
+//2번
+const orderSchema = new mongoose.Schema({
+
+});
+
+//3번
+model.exports = mongoose.model("order", orderSchema)
+//=뒤에 있는 내용부터 쓰고 앞에 model.exports를 쓴다.

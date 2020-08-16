1051590a57e30ecd56b80e7fe52a028ec2b3f873
chaeeun <chaeeun77@naver.com>
2020-08-16 20:03:02 +0900

    create product to MongoDB

--- models/orders.js
+++ models/orders.js
@@ -2,10 +2,10 @@
 const mongoose = require('mongoose'); //mongoose
 
 //2번
-const orderSchema = new mongoose.Schema({
+const orderSchema = mongoose.Schema({
 
 });
 
 //3번
-model.exports = mongoose.model("order", orderSchema)
+module.exports = mongoose.model("order", orderSchema)
 //=뒤에 있는 내용부터 쓰고 앞에 model.exports를 쓴다.
--- models/product.js
+++ models/product.js
@@ -2,13 +2,13 @@
 const mongoose = require('mongoose');
 
 //2번
-const productShcema = new mongoose.Shcema({
-    name: String;
-    price: Number;
+const productShcema = mongoose.Schema({
+    name: String,
+    price: Number
 }); //스키마 : 엑셀에서 칸 같은거
 
 //3번
-model.exports = mongoose.model("product", productSchema)
+module.exports = mongoose.model("product", productShcema)
 //model화 해서 export해서 mongoose의 model을 새로 만들고 이름이 product! 여기에 productSchema의 내용을 넣음
 //DB 설계시 1번-3번 순서로 만든다.
 

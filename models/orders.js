66b945862c3a7f06b24b6209e9a34539e3a831b4
chaeeun <chaeeun77@naver.com>
2020-08-23 19:32:20 +0900

    add order model

--- models/orders.js
+++ models/orders.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose'); //mongoose
 
 //2번
 const orderSchema = mongoose.Schema({
-
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: 1 //기본 설정 값이 1이다. 주문을 할때 처음 설정값이 1이다.
+    }
 });
 
 //3번

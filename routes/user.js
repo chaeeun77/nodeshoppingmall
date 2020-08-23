3cd41fd0cf855e39f395199dabafa48332087af0
chaeeun <chaeeun77@naver.com>
2020-08-23 21:39:32 +0900

    user sign up #1(1단계)

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,30 @@
 //1번
 const express = require('express');
 const router = express.Router();
+const userModel = require("../models/user")
 
 // 회원가입
-router.post('/register', (req, res) => {})
+router.post('/register', (req, res) => {
+    const newUser = new userModel({
+        username: req.body.name,
+        email: req.body.useremail,
+        password: req.body.password
+    })
 
-
-
+    newUser
+        .save()
+        .then(user => {
+            res.json({
+                message: "saved data",
+                userInfo: user
+            })
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
+})
 
 //로그인
 router.post('/login', (req, res) => {})
